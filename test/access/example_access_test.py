ff317ff8176c276e266f1adc9dfbc30f35392c1d
Jiayou Song <--replace-all>
2023-11-14 15:17:36 +0000

    changed testing framework

---
+++ test/access/example_access_test.py
@@ -0,0 +1,14 @@
+# Change directory to root for importing the fynesse module
+import os, sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+import fynesse.access
+
+def func(x):
+    return x + 2
+
+def test_answer():
+    assert func(3) == 5
+
+def test_imported():
+    assert dir(fynesse.access) == dir(fynesse.access)
---
+++ test/address_tests.py
@@ -0,0 +1,3 @@
+import pytest
+
+pytest.main(["-x", "test/address"])
