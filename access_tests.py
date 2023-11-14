af433f279e079c3f16dc924cdb8a509f6527d356
Jiayou Song <--replace-all>
2023-11-14 14:11:30 +0000

    add test modules

--- access_tests.py
+++ access_tests.py
@@ -3,5 +3,5 @@
 import nose
 import warnings
 
-# THese tests should be checking the known access problems, such as whether an excel file has turned a gene to a date or has more than 1,048,576 rows.
+# These tests should be checking the known access problems, such as whether an excel file has turned a gene to a date or has more than 1,048,576 rows.
 nose.main("fynesse", defaultTest="fynesse/tests/access", argv=["", ""])
